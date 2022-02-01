638f6c4ecb30f4fa9989c146d36347a1e8068610
Matthijs Brouwer <matthijs@eight.nl>
2022-02-01 11:33:52 +0100

    rooms setup check

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -25,7 +25,10 @@ const ContentPage = () => {
   
   const socket:any = useSocket('http://localhost:3001')
   const [some, setSome] = useState('nothing yet');
-  const [room , setRoom] = useState('dark');
+  const [joinRoomName , setJoinRoomName] = useState('some room');
+  const [createRoomName , setCreateRoomName] = useState('some room');
+  const [msg , setMsg] = useState('a message');
+  const [name , setName] = useState('John Doe');
 
   useEffect(() => {
     function handleEvent(payload:any) {
@@ -33,21 +36,46 @@ const ContentPage = () => {
       setSome(JSON.stringify(payload));
     }
     if (socket) {
-      socket.on('now', handleEvent)
+      socket.on('now', handleEvent);
+      socket.on('view', handleEvent);
     }
   }, [socket, setSome]);
 
-  const sendShizzles = () => {
-    console.log('sending shizzles', room);
-    socket.emit("test", room);
+  const joinRoom = () => {
+    console.log('Joining room', joinRoomName);
+    socket.emit("joinRoom", joinRoomName);
   }
+
+  const createRoom = () => {
+    console.log('Creating room', createRoomName);
+    socket.emit("createRoom", createRoomName);
+  }
+
+  const sendMsg = () => {
+    console.log('Sending msg', msg);
+    socket.emit("msg", {room: joinRoomName, msg: msg});
+  }
+  
 
   return (
     <div>
-      <h1>Create room!</h1>
-      <div>{some}</div>
-      <input type="text" name="room" onChange={event => setRoom(event.target.value)} value={room} />
-      <button onClick={sendShizzles}>Send The Shizzles</button>
+      <h1>Socket concept tests</h1>
+      <section>
+        <h2>Create room!</h2>
+        <input type="text" name="createRoom" onChange={e => setCreateRoomName(e.target.value)} value={createRoomName} />
+        <button onClick={createRoom}>create room</button>
+      </section>
+      <section>
+        <h2>Join room</h2>
+        <input type="text" name="joinRoom" onChange={e => setJoinRoomName(e.target.value)} value={joinRoomName} />
+        <input type="text" name="name" onChange={e => setName(e.target.value)} value={name} />
+        <button onClick={joinRoom}>join room</button>
+      </section>
+      <section>
+        <h2>Send message to joined room</h2>
+        <input type="text" name="msg" onChange={e => setMsg(e.target.value)} value={msg} />
+        <button onClick={sendMsg}>Send</button>
+      </section>
       <pre>{some}</pre>
     </div>
   )
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,23 +6,69 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:8000"],
   },
 });
 
 console.log("--- Pedago Server started at port 3001 ---");
 
+type gameType = any;
+const games = {} as { [key: string]: gameType };
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  // does this emit to all users?
   socket.emit("now", {
     message: "Hello you have connected",
   });
 
-  //test
+  // test
   socket.on("test", (data) => {
     console.log("Test sent to us with data: ", data);
     socket.emit("now", {
       message: "You sent a test",
+    });
+  });
+
+  // create room
+  socket.on("createRoom", (data) => {
+    console.log("User " + socket.id + " wants to create room: ", data);
+    socket.join(data);
+    // string for current timestamp:
+    const timestamp = new Date().getTime().toString();
+    // create room in games object
+    games[data] = {
+      players: [],
+      admin: socket.id,
+      active: true,
+      started: timestamp,
+    };
+    console.log("All current rooms in socket.rooms:", socket.rooms);
+    console.log("gamedata: ", games);
+    socket.emit("now", {
+      message: "You created a room",
+    });
+  });
+
+  // joinRoom
+  socket.on("joinRoom", (data) => {
+    // console.log("joinRoom sent to us with data: ", data);
+    const userId = socket.id;
+    console.log("User " + userId + " wants to join room: ", data);
+    // check if room exists
+    let msg;
+    if (games[data]) {
+      socket.join(data);
+      games[data].players.push(userId);
+      msg = "You joined room " + data;
+    } else {
+      msg = "Room " + data + " does not exist";
+    }
+    console.log("gamedata: ", games);
+
+    socket.emit("now", {
+      message: msg,
     });
   });
 
