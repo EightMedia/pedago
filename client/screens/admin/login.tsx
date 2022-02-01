97afd63800afeb055a6c6967632d9ae5db282606
Matthijs Brouwer <matthijs@eight.nl>
2022-02-01 09:51:24 +0100

    setting up test

---
+++ client/screens/admin/login.tsx
@@ -0,0 +1,12 @@
+
+
+const ContentPage = () => {
+  const thing = ['ding','dong'];
+  return (
+    <div>
+      <h1>Hello page!</h1>
+      <p>{thing[0]}</p>
+    </div>
+  )
+}
+export default ContentPage;
