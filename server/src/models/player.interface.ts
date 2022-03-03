c475d78933fde4d88e4b27edae311f656df927b4
René Hasert <rene@eight.nl>
2022-03-03 21:32:06 +0100

    workspaces

--- server/src/models/player.interface.ts
+++ server/src/models/player.interface.ts
@@ -1,13 +1,12 @@
-import { Category } from "./category.enum";
 import { Group } from "./group.interface";
 import { Round } from "./round.interface";
 import { View } from "./view.enum";
 
 export interface Player {
-    playerId: string;
-    name: string;
-    group: Group;
-    roomId: string;
-    rounds: Round[]
-    view: View;
+  playerId: string;
+  name: string;
+  group: Group;
+  roomId: string;
+  rounds: Round[];
+  view: View;
 }
