6c1cc2f62507979e628980ab9b6d53d1b5001721
René Hasert <rene@eight.nl>
2022-03-03 22:40:46 +0100

    remove duplicates

--- server/src/models/option.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Options {
-    timer: boolean;
-    inGroups: boolean;
-    groups?: string[]
-}
--- server/src/models/role.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum Role {
-    Docent,
-    Trainer,
-    Manager,
-    Anders
-}
