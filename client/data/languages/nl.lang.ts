0c03a2e506aa9958865113c7f89799bd65ace6ae
René Hasert <rene@eight.nl>
2022-03-07 10:43:48 +0100

    prettier

--- client/data/languages/nl.lang.ts
+++ client/data/languages/nl.lang.ts
@@ -1,13 +1,13 @@
-import { DataTranslation } from 'models';
+import { DataTranslation } from "models";
 
 export const NL: DataTranslation = {
-    landing: {
-        title: 'Ontdek jouw idealen als leraar',
-        description:
-            'Leer jouw eigen pedagogisch krachten kennen met dit spel. Je speelt met andere spelers, samen bespreken jullie de resultaten.',
-        input: 'Voer spelcode in',
-        button: 'Meedoen aan spel',
-        create: 'Spel aanmaken',
-        asAdmin: 'als beheerder',
-    },
+  landing: {
+    title: "Ontdek jouw idealen als leraar",
+    description:
+      "Leer jouw eigen pedagogisch krachten kennen met dit spel. Je speelt met andere spelers, samen bespreken jullie de resultaten.",
+    input: "Voer spelcode in",
+    button: "Meedoen aan spel",
+    create: "Spel aanmaken",
+    asAdmin: "als beheerder",
+  },
 };
--- client/pages/game/[gameCode].tsx
+++ client/pages/game/[gameCode].tsx
@@ -7,61 +7,61 @@ import GameLobby from '../../views/game/Lobby/GameLobby';
 import GameResult from '../../views/game/Result/GameResult';
 import GameWizard from '../../views/game/Wizard/GameWizard';
 
-function useSocket( url: string ) {
-  const [ socket, setSocket ] = useState<Socket | null>( null );
+function useSocket(url: string) {
+    const [socket, setSocket] = useState<Socket | null>(null);
 
-  useEffect( () => {
-    const socketIo = io( url );
+    useEffect(() => {
+        const socketIo = io(url);
 
-    setSocket( socketIo );
+        setSocket(socketIo);
 
-    function cleanup() {
-      socketIo.disconnect();
-    }
-    return cleanup;
+        function cleanup() {
+            socketIo.disconnect();
+        }
+        return cleanup;
 
-    // should only run once and not on every re-render,
-    // so pass an empty array
-  }, [] );
+        // should only run once and not on every re-render,
+        // so pass an empty array
+    }, []);
 
-  return socket;
+    return socket;
 }
 
 const GameMain = () => {
-  const socket: any = useSocket( 'http://localhost:3001' );
-  const [ view, setView ] = useState( initialViewState );
+    const socket: any = useSocket('http://localhost:3001');
+    const [view, setView] = useState(initialViewState);
 
-  const handleClick = ( value: ViewName ): void => {
-    socket.emit( 'to', { name: value } );
-  };
+    const handleClick = (value: ViewName): void => {
+        socket.emit('to', { name: value });
+    };
 
-  useEffect( () => {
-    if ( socket ) {
-      socket.on( 'to', ( v: ViewState ) => setView( v ) );
-    }
-  }, [ socket, view ] );
-
-  const router = useRouter();
-  const gameCode = router.query.gameCode;
-
-  return (
-    <div>
-      <h1>Joining game { gameCode }</h1>
-      { ( () => {
-        switch ( view.name ) {
-          case ViewName.Wizard:
-            return <GameWizard handleClick={ handleClick } />;
-          case ViewName.Lobby:
-            return <GameLobby handleClick={ handleClick } />;
-          case ViewName.Game:
-            return <GameGame handleClick={ handleClick } />;
-          case ViewName.Result:
-            return <GameResult handleClick={ handleClick } />;
-          default:
-            return null;
+    useEffect(() => {
+        if (socket) {
+            socket.on('to', (v: ViewState) => setView(v));
         }
-      } )() }
-    </div>
-  );
+    }, [socket, view]);
+
+    const router = useRouter();
+    const gameCode = router.query.gameCode;
+
+    return (
+        <div>
+            <h1>Joining game {gameCode}</h1>
+            {(() => {
+                switch (view.name) {
+                    case ViewName.Wizard:
+                        return <GameWizard handleClick={handleClick} />;
+                    case ViewName.Lobby:
+                        return <GameLobby handleClick={handleClick} />;
+                    case ViewName.Game:
+                        return <GameGame handleClick={handleClick} />;
+                    case ViewName.Result:
+                        return <GameResult handleClick={handleClick} />;
+                    default:
+                        return null;
+                }
+            })()}
+        </div>
+    );
 };
 export default GameMain;
