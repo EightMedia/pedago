6f6df296574fad7c523aa7540d9488adbae39967
René Hasert <rene@eight.nl>
2022-03-07 10:46:53 +0100

    compiled ts

---
+++ lib/models/category.enum.d.ts
@@ -0,0 +1,8 @@
+export declare enum Category {
+    Caring = 0,
+    Personal = 1,
+    Contextual = 2,
+    Critical = 3,
+    Functional = 4,
+    Psychological = 5,
+}
---
+++ lib/models/sector.enum.d.ts
@@ -0,0 +1,10 @@
+export declare enum Sector {
+    PO = 0,
+    VO = 1,
+    MBO = 2,
+    HBO = 3,
+    WO = 4,
+    PrMaHBO = 5,
+    AcMa = 6,
+    Anders = 7,
+}
