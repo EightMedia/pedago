f5a1a329dd08fb55d4e802d7c6f6e415da3e833b
René Hasert <rene@eight.nl>
2022-03-08 12:58:30 +0100

    conflicts

--- lib/models/language.enum.d.ts
+++ lib/models/language.enum.d.ts
@@ -1,9 +1,4 @@
 export declare enum Language {
-<<<<<<< HEAD
-    NL = "NL",
-    EN = "EN"
-=======
     NL = 'NL',
     EN = 'EN',
->>>>>>> main
 }
--- lib/models/role.enum.d.ts
+++ lib/models/role.enum.d.ts
@@ -2,9 +2,5 @@ export declare enum Role {
     Docent = 0,
     Trainer = 1,
     Manager = 2,
-<<<<<<< HEAD
-    Anders = 3
-=======
     Anders = 3,
->>>>>>> main
 }
