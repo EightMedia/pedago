fda907fbd96f5ffca6e0ad64a84cd67f9722b3b4
René Hasert <rene@eight.nl>
2022-03-08 13:01:31 +0100

    tsc

--- lib/models/view-state.interface.d.ts
+++ lib/models/view-state.interface.d.ts
@@ -1,14 +1,13 @@
 export declare enum ViewName {
-  Wizard,
-  Lobby,
-  Game,
-  Result,
-  SelectGroup,
-  InfoScreen,
+    Wizard = 0,
+    Lobby = 1,
+    Game = 2,
+    Result = 3
 }
 export interface ViewState {
-  name: ViewName;
-  data: ViewData;
+    name: ViewName;
+    data: ViewData;
 }
 export declare const initialViewState: ViewState;
-export interface ViewData {}
+export interface ViewData {
+}
