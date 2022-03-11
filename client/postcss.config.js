9f10f2fe598c7a3d2c779d66ff7ab84c7d23e8cd
Matthijs Brouwer <matthijs@eight.nl>
2022-03-11 13:41:09 +0100

    fixed postcss in frontend rendering

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,6 +2,29 @@
 module.exports = {
   plugins: {
     "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 0,
+      features: {
+        "custom-properties": {
+          importFrom: "./lib/styles/vars.css",
+          preserve: true,
+        },
+        "custom-media-queries": {
+          importFrom: "./lib/styles/globals.css",
+        },
+        "nesting-rules": true,
+      },
+    },
   },
 };
+
+// // yarn add postcss-flexbugs-fixes postcss-preset-env -D
+// module.exports = {
+//   plugins: {
+//     "postcss-flexbugs-fixes": {},
+//     "postcss-preset-env": {},
+//   },
+// };
