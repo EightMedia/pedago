db867e8363619ac421ab60f03299b9d49f74f726
Matthijs Brouwer <matthijs@eight.nl>
2022-03-11 13:52:41 +0100

    stack story

--- client/lib/layouts/Stack/Stack.stories.tsx
+++ client/lib/layouts/Stack/Stack.stories.tsx
@@ -4,11 +4,24 @@ import { Stack } from "./Stack";
 import { StackData } from "./Stack.data";
 
 export default {
-  title: "Components/Stack",
+  title: "Layouts/Stack",
   component: Stack,
 } as ComponentMeta<typeof Stack>;
 
 const Template: ComponentStory<typeof Stack> = (args) => <Stack {...args} />;
+
+const DemoStack = () => {
+  return (
+    <>
+      <div style={{ background: "blue", height: "30px" }}></div>
+      <div style={{ background: "blue", height: "30px" }}></div>
+      <div style={{ background: "blue", height: "30px" }}></div>
+      <div style={{ background: "blue", height: "30px" }}></div>
+      <div style={{ background: "blue", height: "30px" }}></div>
+      <div style={{ background: "blue", height: "30px" }}></div>
+    </>
+  );
+};
 
 // story
 export const Primary = Template.bind({});
@@ -19,4 +32,4 @@ Primary.parameters = {
   },
 };
 Primary.storyName = "Stack";
-Primary.args = { ...StackData };
+Primary.args = { children: DemoStack() };
