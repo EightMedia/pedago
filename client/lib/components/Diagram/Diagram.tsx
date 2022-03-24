31b4ef16d9745fe24d6e5ed520f4bf8b5b881bd1
Matthijs Brouwer <matthijs@eight.nl>
2022-03-24 14:04:32 +0100

    styling

--- client/lib/components/Diagram/Diagram.tsx
+++ client/lib/components/Diagram/Diagram.tsx
@@ -6,10 +6,14 @@ import {
   RadialLinearScale,
   Tooltip,
 } from "chart.js";
+import cx from "classnames";
 import { memo, useContext } from "react";
 import { Radar } from "react-chartjs-2";
 import { LanguageContext } from "../../../contexts/LanguageContext";
+import { Shape } from "../Shape";
+import styles from "./Diagram.module.css";
 import { DiagramType } from "./Diagram.types";
+
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip);
 
 const findCategoryTitles = () => {
@@ -32,21 +36,30 @@ const DiagramComponent = ({
         label: primaryLabel,
         data: primary,
         backgroundColor: "rgba(255, 255, 255, 0.8)",
-        borderColor: "rgba(255, 255, 255, 1)",
-        borderWidth: 1,
+        borderColor: "rgba(255, 255, 255, 0.8)",
       },
       {
         label: secondaryLabel,
         data: secondary,
         backgroundColor: "rgba(255, 255, 255, 0.2)",
-        borderColor: "rgba(255, 255, 255, 1)",
-        borderWidth: 1,
+        borderColor: "rgba(255, 255, 255, 0.2)",
       },
     ],
   };
   const options = {
+    elements: {
+      line: {
+        borderWidth: 2,
+      },
+      point: {
+        radius: 0,
+      },
+    },
     scales: {
       r: {
+        pointLabels: {
+          display: false,
+        },
         max: 36,
         min: 0,
         ticks: {
@@ -54,17 +67,27 @@ const DiagramComponent = ({
           display: false,
         },
         angleLines: {
-          lineWidth: 2,
           color: "rgba(255,255,255,0.15)",
         },
         grid: {
-          lineWidth: 2,
           color: "rgba(255,255,255,0.15)",
         },
       },
     },
   };
-  return <Radar data={data} options={options} />;
+  return (
+    <div className={styles.wrapper}>
+      {labels.map((label, index) => (
+        <div key={index} className={cx(styles["label" + index], styles.label)}>
+          {label.split(" ").map((word) => (
+            <div key={index}>{word}</div>
+          ))}
+          <Shape category={index} />
+        </div>
+      ))}
+      <Radar data={data} options={options} />;
+    </div>
+  );
 };
 
 export const Diagram = memo(DiagramComponent);
