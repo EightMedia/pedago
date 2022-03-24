7e15b6c6f07d05ce5052abd496d0c58fa2257a74
Matthijs Brouwer <matthijs@eight.nl>
2022-03-24 16:43:31 +0100

    fixed storybook

--- client/lib/components/SortList/SortList.data.ts
+++ client/lib/components/SortList/SortList.data.ts
@@ -3,4 +3,8 @@ import { SortListType } from "./SortList.types";
 
 export const SortListData: SortListType = {
   cards: NL.rounds[0].cards,
+  handleSortOrder: (items) => {
+    alert("We've sorted it out: " + JSON.stringify(items));
+    return;
+  },
 };
