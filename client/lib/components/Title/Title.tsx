0a1ea1b0e33bfedaeb9dd7d34f9e170cd2a402ee
Matthijs Brouwer <matthijs@eight.nl>
2022-03-29 10:11:33 +0200

    updated title to show h1

--- client/lib/components/Title/Title.tsx
+++ client/lib/components/Title/Title.tsx
@@ -1,9 +1,19 @@
-import { memo } from "react";
+import cx from "classnames";
+import { createElement, memo } from "react";
 import styles from "./Title.module.css";
 import { TitleType } from "./Title.types";
 
-const TitleComponent = ({ children }: TitleType) => {
-  return <h2 className={styles.title}>{children}</h2>;
+const TitleComponent = ({
+  children,
+  size = "md",
+  element = "h2",
+}: TitleType) => {
+  // render element based on element var
+  return createElement(
+    element,
+    { className: cx(styles.title, styles[size]) },
+    children
+  );
 };
 
 export const Title = memo(TitleComponent);
