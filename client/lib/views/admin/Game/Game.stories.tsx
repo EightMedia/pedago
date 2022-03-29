10461ba776a04ab2543b6b4b7bda6208c80b7a76
Matthijs Brouwer <matthijs@eight.nl>
2022-03-29 15:53:49 +0200

    added screenshots

--- client/lib/views/admin/Game/Game.stories.tsx
+++ client/lib/views/admin/Game/Game.stories.tsx
@@ -10,7 +10,7 @@ export default {
 
 const Template: ComponentStory<typeof Game> = (args) => <Game {...args} />;
 
-// story
+// game round
 export const Primary = Template.bind({});
 Primary.parameters = {
   layout: "fullscreen",
@@ -19,8 +19,38 @@ Primary.parameters = {
   },
   design: {
     type: "figma",
-    url: "",
+    url: "https://www.figma.com/file/DZM2PnJJJuuqsxjO8tv8Kn/Pedago?node-id=495%3A9643",
   },
 };
 Primary.storyName = "Game";
 Primary.args = { ...GameData };
+
+// confirm skip
+export const ConfirmSkip = Template.bind({});
+ConfirmSkip.parameters = {
+  layout: "fullscreen",
+  viewport: {
+    defaultViewport: "Desktop",
+  },
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/DZM2PnJJJuuqsxjO8tv8Kn/Pedago?node-id=618%3A4525",
+  },
+};
+ConfirmSkip.storyName = "Confirm skip round";
+ConfirmSkip.args = { ...GameData };
+
+// next round teaser
+export const NextRound = Template.bind({});
+NextRound.parameters = {
+  layout: "fullscreen",
+  viewport: {
+    defaultViewport: "Desktop",
+  },
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/DZM2PnJJJuuqsxjO8tv8Kn/Pedago?node-id=618%3A4205",
+  },
+};
+NextRound.storyName = "Next round";
+NextRound.args = { ...GameData };
