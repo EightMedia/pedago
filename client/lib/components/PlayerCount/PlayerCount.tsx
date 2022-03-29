c2b506e4c1141faf00f58eaaee501c2e049fa69a
Matthijs Brouwer <matthijs@eight.nl>
2022-03-29 15:42:42 +0200

    admin lobby

---
+++ client/lib/components/PlayerCount/PlayerCount.tsx
@@ -0,0 +1,19 @@
+import cx from "classnames";
+import { memo } from "react";
+import { Icon, IconsEnum } from "../Icon/Icon";
+import styles from "./PlayerCount.module.css";
+import { PlayerCountType } from "./PlayerCount.types";
+
+const PlayerCountComponent = ({
+  players,
+  variation = "dark",
+}: PlayerCountType) => {
+  return (
+    <div className={cx(styles.box, styles[variation])}>
+      <Icon icon={IconsEnum.Person} />
+      {players}
+    </div>
+  );
+};
+
+export const PlayerCount = memo(PlayerCountComponent);
--- client/lib/views/admin/Lobby/Lobby.types.ts
+++ client/lib/views/admin/Lobby/Lobby.types.ts
@@ -1,5 +1,18 @@
+import { RoomDto } from "models";
 import { PlayerGroupType } from "../../../components/PlayerGroup/PlayerGroup.types";
 
+export enum LobbyStep {
+  Info,
+  Lobby,
+}
+
 export type LobbyType = {
-  lobbyGroups: PlayerGroupType[];
+  room: {
+    roomCode: RoomDto["roomCode"];
+    id: RoomDto["id"];
+    groups: PlayerGroupType[];
+  };
+  initialStep?: LobbyStep;
+  handleStart: () => void;
+  handleInfo: () => void;
 };
