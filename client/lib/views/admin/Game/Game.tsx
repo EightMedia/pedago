96bed264f45803cccab05e042af8282d48914c66
René Hasert <rene@eight.nl>
2022-03-30 12:01:16 +0200

    stopround

--- client/lib/views/admin/Game/Game.tsx
+++ client/lib/views/admin/Game/Game.tsx
@@ -1,10 +1,17 @@
 import { ViewName } from "models";
 import { PlayerStatus } from "models/lib/models/player-status.enum";
-import { memo } from "react";
+import { memo, useContext } from "react";
+import { RoomContext } from "../../../../contexts/RoomContext";
+import { SocketContext } from "../../../../contexts/SocketContext";
 import { Page } from "../../../components/Page";
 import { GameType } from "./Game.types";
 
-const GameComponent = ({ handleView, teams, stopRound }: GameType) => {
+const GameComponent = ({ handleView, teams }: GameType) => {
+  const socket = useContext(SocketContext);
+  const room = useContext(RoomContext);
+
+  const stopRound = () => {}
+
   return (
     <Page>
       <h2>Game</h2>
@@ -27,7 +34,13 @@ const GameComponent = ({ handleView, teams, stopRound }: GameType) => {
         In progress
         {teams &&
           teams
-            .filter((t) => t.every((p) => p.status === PlayerStatus.InProgress || p.status === PlayerStatus.Discuss))
+            .filter((t) =>
+              t.every(
+                (p) =>
+                  p.status === PlayerStatus.InProgress ||
+                  p.status === PlayerStatus.Discuss
+              )
+            )
             ?.map((team, index) => (
               <div key={index}>
                 <div>{index + 1}</div>
--- client/lib/views/admin/Game/Game.types.ts
+++ client/lib/views/admin/Game/Game.types.ts
@@ -2,7 +2,6 @@ import { Player, ViewState } from "models";
 
 export type GameType = {
     handleView: (view: ViewState) => void;
-    stopRound: () => void;
     teams: Player[][];
     round: number;
 };
