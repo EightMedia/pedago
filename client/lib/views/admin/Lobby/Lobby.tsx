800ff5439f5e2c6a9ea69fe2bbc6fb06a0d46526
Matthijs Brouwer <matthijs@eight.nl>
2022-03-30 11:38:53 +0200

    fix storybook, added room as property

--- client/lib/views/admin/Lobby/Lobby.tsx
+++ client/lib/views/admin/Lobby/Lobby.tsx
@@ -1,27 +1,34 @@
 import { AdminEvent, SocketCallback } from "models";
 import { memo, useContext, useState } from "react";
-import { RoomContext } from "../../../../contexts/RoomContext";
 import { SocketContext } from "../../../../contexts/SocketContext";
 import { LobbyStep, LobbyType } from "./Lobby.types";
 import { LobbyInfo } from "./LobbyInfo.scene";
 import { LobbyLobby } from "./LobbyLobby.scene";
 
-const LobbyComponent = ({ groups, initialStep }: LobbyType) => {
+const LobbyComponent = ({
+  room,
+  groups,
+  initialStep,
+  handleStart,
+}: LobbyType) => {
   const [step, setStep] = useState<LobbyStep>(initialStep as LobbyStep);
-  const room = useContext(RoomContext);
   const socket = useContext(SocketContext);
 
-  const handleStart = () => {
-    socket?.emit(AdminEvent.StartGame, room?.id, (r: SocketCallback) => {
-      console.log(r);
-    });
-  };
+  if (!handleStart) {
+    handleStart = () => {
+      socket?.emit(AdminEvent.StartGame, room?.id, (r: SocketCallback) => {
+        console.log(r);
+      });
+    };
+  }
+
   switch (step) {
     case LobbyStep.Info:
       return <LobbyInfo handleClick={() => setStep(LobbyStep.Lobby)} />;
     case LobbyStep.Lobby:
       return (
         <LobbyLobby
+          room={room}
           groups={groups}
           handleStart={handleStart}
           handleInfo={() => setStep(LobbyStep.Info)}
