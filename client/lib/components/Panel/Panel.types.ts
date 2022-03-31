e14907b10a3531c7e2af8b98656e7f8eb022be02
Matthijs Brouwer <matthijs@eight.nl>
2022-03-31 08:50:54 +0200

    Panel fixes (tiny thingy) (#46)
    * panel fixes
    
    * panel story

--- client/lib/components/Panel/Panel.types.ts
+++ client/lib/components/Panel/Panel.types.ts
@@ -2,5 +2,4 @@ import { ReactNode } from "react";
 
 export type PanelType = {
   children: ReactNode;
-  space?: "sm" | "lg";
 };
--- client/lib/views/admin/Game/GameOnboarding.tsx
+++ client/lib/views/admin/Game/GameOnboarding.tsx
@@ -60,7 +60,7 @@ export const GameInfo = ({ title }: { title: string }) => {
 export const GameOnboarding = ({ handleOk }: { handleOk: () => void }) => {
   return (
     <Page>
-      <Panel space="lg">
+      <Panel>
         <GameInfo title="Voor we beginnen" />
         <Button onClick={handleOk}>Ik snap het</Button>
       </Panel>
