a5942b5ee895d3b3bab91bc792ec907ecdd10cb9
Matthijs Brouwer <matthijs@eight.nl>
2022-04-05 09:47:52 +0200

    panel width prop

--- client/lib/components/Panel/Panel.tsx
+++ client/lib/components/Panel/Panel.tsx
@@ -3,8 +3,8 @@ import { memo } from "react";
 import styles from "./Panel.module.css";
 import { PanelType } from "./Panel.types";
 
-const PanelComponent = ({ children }: PanelType) => {
-  return <div className={cx(styles.panel)}>{children}</div>;
+const PanelComponent = ({ children, width = "md" }: PanelType) => {
+  return <div className={cx(styles.panel, styles[width])}>{children}</div>;
 };
 
 export const Panel = memo(PanelComponent);
