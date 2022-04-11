7156c8d767c4d1fd6e1583e5a833073e134a38cb
Matthijs Brouwer <matthijs@eight.nl>
2022-04-11 15:28:00 +0200

    fixed render error

--- client/lib/components/Page/Page.tsx
+++ client/lib/components/Page/Page.tsx
@@ -1,11 +1,11 @@
 import cx from "classnames";
-import { isValidElement, memo } from "react";
+import { memo } from "react";
 import styles from "./Page.module.css";
 import { PageSlotType, PageType } from "./Page.types";
 
 export const PageSlot = ({ children, className, location }: PageSlotType) => (
   <div className={cx(styles[location], className)}>
-    {isValidElement(children) ? children : <p>{children}</p>}
+    {typeof children === "object" ? children : <p>{children}</p>}
   </div>
 );
 
@@ -13,7 +13,7 @@ const PageComponent = ({ children, background = 1 }: PageType) => {
   return (
     <div className={cx(styles.page, styles["bg" + background])}>
       <div className={styles.container}>
-        {isValidElement(children) ? children : <p>{children}</p>}
+        {typeof children === "object" ? children : <p>{children}</p>}
       </div>
     </div>
   );
