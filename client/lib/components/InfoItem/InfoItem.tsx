241396dc38b4deaa926bec24bdb79412ef615c69
Matthijs Brouwer <matthijs@eight.nl>
2022-04-14 16:42:22 +0200

    enable icon with id 0

--- client/lib/components/InfoItem/InfoItem.tsx
+++ client/lib/components/InfoItem/InfoItem.tsx
@@ -8,7 +8,9 @@ import { InfoItemType } from "./InfoItem.types";
 const InfoItemComponent = ({ title, text, icon }: InfoItemType) => {
   return (
     <div className={styles.item}>
-      {icon && <Icon icon={icon} className={styles.icon} infoIcon />}
+      {typeof icon !== "undefined" && (
+        <Icon icon={icon} className={styles.icon} infoIcon />
+      )}
       <div className={styles.text}>
         <TextTitle>{title}</TextTitle>
         <Text tone="light">{text}</Text>
