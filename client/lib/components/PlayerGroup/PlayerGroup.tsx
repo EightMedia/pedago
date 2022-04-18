3bc733f59c68b56a1e4b191420f10769420b8c2b
Matthijs Brouwer <matthijs@eight.nl>
2022-04-18 07:08:22 +0200

    change to group button

--- client/lib/components/PlayerGroup/PlayerGroup.tsx
+++ client/lib/components/PlayerGroup/PlayerGroup.tsx
@@ -2,18 +2,24 @@ import cx from "classnames";
 import { memo, useContext } from "react";
 import { LanguageContext } from "../../../contexts/LanguageContext";
 import { Center } from "../../layouts/Center";
+import { Button } from "../Button";
 import { GlassPanel } from "../GlassPanel";
 import { Player } from "../Player/Player";
 import { PlayerCount } from "../PlayerCount";
 import styles from "./PlayerGroup.module.css";
 import { PlayerGroupType } from "./PlayerGroup.types";
 
-export const PlayerGroupComponent = ({ name, players }: PlayerGroupType) => {
-  const text = useContext(LanguageContext).adminLobby.playerGroup;
+export const PlayerGroupComponent = ({
+  name,
+  players,
+  counter,
+  handleGroupChange,
+}: PlayerGroupType) => {
+  const text = useContext(LanguageContext);
   return (
     <GlassPanel>
       <Center>
-        <PlayerCount players={players?.length as number} />
+        {counter && <PlayerCount players={players?.length as number} />}
         <div className={cx(styles.groupName)}>{name}</div>
         {players?.length ? (
           <div className={cx(styles.players)}>
@@ -23,6 +29,15 @@ export const PlayerGroupComponent = ({ name, players }: PlayerGroupType) => {
           </div>
         ) : (
           <p>{text.waiting}</p>
+        )}
+        {handleGroupChange && (
+          <Button
+            variation="line"
+            className={styles.changeButton}
+            onClick={handleGroupChange}
+          >
+            {text.gameLobby.changeToThisGroup}
+          </Button>
         )}
       </Center>
     </GlassPanel>
