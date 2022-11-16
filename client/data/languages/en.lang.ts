a6a79fbc90d7deb5aacaedd90f8d8b2fc33813f5
René Hasert <rene@eight.nl>
2022-11-16 13:10:20 +0100

    friedrich engels

--- client/data/languages/en.lang.ts
+++ client/data/languages/en.lang.ts
@@ -12,4 +12,199 @@ export const EN: Locale = {
     create: "Create a game",
     asAdmin: "as admin",
   },
+  rounds: [
+    {
+      id: 1,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest that...",
+      cards: [
+        {
+          id: "0",
+          title: "They learn to survive in a demanding world.",
+        },
+        {
+          id: "1",
+          title: "They learn to shape the direction of their lives.",
+        },
+        {
+          id: "2",
+          title: "They learn to find their way in modern society.",
+        },
+        {
+          id: "3",
+          title: "They grow up to be self-aware and independent adults.",
+        },
+        {
+          id: "4",
+          title: "They further their education to the highest levels they can.",
+        },
+        {
+          id: "5",
+          title:
+            "They have realistic expectations with regard to what they can achieve in their lives.",
+        },
+      ],
+    },
+    {
+      id: 2,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest to...",
+      cards: [
+        {
+          id: "0",
+          title: "Guide them with care and patience in their learning process.",
+        },
+        {
+          id: "1",
+          title: "Tap into their intrinsic motivation and ambition.",
+        },
+        {
+          id: "2",
+          title: "Design learning environments that reflect real life.",
+        },
+        {
+          id: "3",
+          title: "Acquaint them with different perspectives on reality.",
+        },
+        {
+          id: "4",
+          title: "Direct them towards predetermined learning goals.",
+        },
+        {
+          id: "5",
+          title:
+            "Connect to their cognitive and social abilities and limitations.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest that...",
+      cards: [
+        {
+          id: "0",
+          title:
+            "They develop a sense of self-respect and a positive self-image.",
+        },
+        {
+          id: "1",
+          title: "They learn to develop and make use of their creativity.",
+        },
+        {
+          id: "2",
+          title:
+            "They learn practical life skills in order to participate in modern society.",
+        },
+        {
+          id: "3",
+          title: "They learn to formulate substantiated opinions.",
+        },
+        {
+          id: "4",
+          title:
+            "They master subject matter that is part of the formal curriculum.",
+        },
+        {
+          id: "5",
+          title:
+            "They learn to cope with social, psychological and physical (dis)abilities.",
+        },
+      ],
+    },
+    {
+      id: 4,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest that pupils...",
+      cards: [
+        {
+          id: "0",
+          title:
+            "Develop independence at their own speed and in gradual steps.",
+        },
+        {
+          id: "1",
+          title:
+            "Take their unique developmental progress as a starting point.",
+        },
+        {
+          id: "2",
+          title:
+            "Decide how they can develop in terms of their circumstances in life.",
+        },
+        {
+          id: "3",
+          title:
+            "Be critical about societal expectations of children of different age groups.",
+        },
+        {
+          id: "4",
+          title: "Follow the curriculum as formally planned.",
+        },
+        {
+          id: "5",
+          title:
+            "Be free of obstacles that hinder their natural stages of development.",
+        },
+      ],
+    },
+    {
+      id: 5,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest that...",
+      cards: [
+        {
+          id: "0",
+          title: "The school provides a safe and caring environment.",
+        },
+        {
+          id: "1",
+          title: "The school challenges them to realize their potential.",
+        },
+        {
+          id: "2",
+          title: "The school is connected to the world outside.",
+        },
+        {
+          id: "3",
+          title: "The school is a place where pupils’ voices are heard.",
+        },
+        {
+          id: "4",
+          title:
+            "The school is a place where their learning achievements are highly valued.",
+        },
+        {
+          id: "5",
+          title: "The school provides access to specialized care.",
+        },
+      ],
+    },
+    {
+      id: 6,
+      lead: "In my teaching practice, I consider it in my pupils’ best interest to...",
+      cards: [
+        {
+          id: "0",
+          title: "See them as human beings who are still dependent on adults.",
+        },
+        {
+          id: "1",
+          title: "See them as unique persons with their own points of view.",
+        },
+        {
+          id: "2",
+          title: "See them as participants in the real world.",
+        },
+        {
+          id: "3",
+          title: "See them as a promising new generation.",
+        },
+        {
+          id: "4",
+          title: "See them as learners in a service oriented institution.",
+        },
+        {
+          id: "5",
+          title:
+            "See them as people with specific characteristics and learning needs.",
+        },
+      ],
+    },
+  ],
 };
